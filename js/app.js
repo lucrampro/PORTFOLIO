b71407626f2af2eecb78b016fd02d1011db99671
Ramassamy Lucien <Lucien@Lucien.local>
2019-03-12 12:43:44 +0100

    ajout d'animation et dedinamisme a mon portfolio

--- js/app.js
+++ js/app.js
@@ -83,7 +83,7 @@ $('document').ready(function () {
                         width: "0"
 
                     });
-                    
+
                 }
             });
 
@@ -123,5 +123,219 @@ $('document').ready(function () {
 
     });
 
+    //
+
+    // home 
+
+    TweenMax.to('.lucien', 1, {
+
+        opacity: 1
+    });
+
+
+
+    TweenMax.to('.pdev', 0.8, {
+
+        opacity: 1,
+        delay: 0.3
+    });
+
+
+
+
+    // 
+    var Top = $('.about').offset().top;
+
+    var Height = $('.about').height();
+
+    var scroll = $(window).scrollTop();
+
+    var windowHeight = $(window).height();
+
+
+
+    $(document).on('scroll', function () {
+
+
+        var scroll = $(window).scrollTop();
+
+
+
+
+        if (Math.round(scroll + windowHeight) >= Math.round(Top + Height)) {
+
+
+            TweenMax.to(".citation", 0.8, {
+
+                css: {
+
+                    y: "0",
+
+                    opacity: 1
+
+
+                }
+
+            });
+
+            TweenMax.to(".about .ap", 0.8, {
+
+                css: {
+
+                    y: "0",
+
+                    opacity: 1
+
+
+                },
+                delay: 0.3
+
+            });
+
+            TweenMax.to(".about ul", 0.8, {
+
+                css: {
+
+                    y: "0",
+
+                    opacity: 1
+
+
+                },
+                delay: 0.5
+
+            });
+            TweenMax.to(".about li", 0.8, {
+
+                css: {
+
+                    y: "0",
+
+                    opacity: 1
+
+
+                },
+                delay: 0.7
+
+            });
+
+        }
+
+    });
+
 });
+
+
+// contact page 
+
+TweenMax.to('.page_contact .mail', 0.5, {
+
+    opacity: 1
+});
+
+TweenMax.to('.page_contact .disponible', 0.6, {
+
+    opacity: 1,
+    delay: 0.2
+});
+
+
+TweenMax.to('.github', 1, {
+
+
+    opacity: 0.5,
+    delay: 0.3,
+
+
+});
+
+
+TweenMax.to('.linkedin', 1, {
+
+
+    opacity: 0.5,
+    delay: 0.4,
+
+
+});
+
+// work page
+
+TweenMax.to('.ppolka .number', 0.5, {
+
+    opacity: 0.1
+
+});
+
+TweenMax.to('.ppolka img', 0.5, {
+
+    width: '300px',
+    delay: 0.3
+
+});
+
+
+TweenMax.to('.ppolka a', 0.5, {
+
+    opacity: 1,
+    delay: 0.8
+
+});
+
+
+
+
+var scensoHeight = $('.sscenso').height();
+
+var scensoTop = $('.sscenso').offset().top;
+
+var scroll = $(window).scrollTop();
+
+var windowHeight = $(window).height();
+
+
+
+$(document).on('scroll', function () {
+
+
+    var scroll = $(window).scrollTop();
+
+
+    if (Math.round(scroll + windowHeight) >= Math.round(scensoTop + scensoHeight)) {
+
+
+
+        TweenMax.to('.sscenso .number', 0.5, {
+
+            opacity: 0.1
+
+        });
+
+        TweenMax.to('.sscenso img', 0.5, {
+
+            width: '300px',
+            delay: 0.3
+
+        });
+
+
+        TweenMax.to('.sscenso a', 0.5, {
+
+            opacity: 1,
+            delay: 0.8
+
+        });
+
+
+    }
+});
+
+// projet page 
+
+TweenMax.to('.projet p', 0.5,{
+    
+    color: "#345676"
+});
+
+
 //# sourceMappingURL=app.js.map
